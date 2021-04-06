dcd1a1157a804d60b138b4cc3d0c43b752d2b1dc
Daniel Mueller <danielpetermueller@gmail.com>
2021-04-06 12:09:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,22 +3,18 @@ function add(a,b){
   number=a+b;
   return number;
 }
-var number;
 function subtract(a,b){
   number=a-b;
   return number;
 }
-var number;
 function multiply(a,b){
   number=a*b;
   return number;
 }
-var number;
 function divide(a,b){
   number=a/b;
   return number;
 }
-var number;
 function inc(a){
   number=a+1;
   return number;

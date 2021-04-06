878568988d15d49b8c82b7cbc66aae9eaa0bac1a
Daniel Mueller <danielpetermueller@gmail.com>
2021-04-06 12:01:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,30 +1,4 @@
-add(1,80)
-subtract(60,40)
-multiply(2,3.4)
-divide(5.0,2.5)
-
-var number = 10
-
-function add5(){
-  number+=5
-}
-function divideBy3(){
-  number /=3
-}
-divideBy3()
-
-console.log(number)
-
-add5()
-
-console.log(number)
-
-number=10
-
-add5()
-
-console.log(number)
-
-divideBy3()
-
-console.log(number)
+add();
+subtract();
+multiply();
+divide();
